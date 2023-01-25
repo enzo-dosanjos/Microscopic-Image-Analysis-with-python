d257780bb2569e8ff12e72c72262c3dccca369a8
enzo-dosanjos <colinterz@gmail.com>
2023-01-25 13:20:24 +0100

    including the morphological operation in the main program

--- ImageAnalysisMain.py
+++ ImageAnalysisMain.py
@@ -5,9 +5,10 @@ Created on Tue Jan 24 21:45:05 2023
 @author: Enzo
 """
 
-import cv2
-from ImageAnalysisFct import *
-from skimage import io
+import cv2 #to install opencv: pip install opencv-python
+from termcolor import colored #to install termcolor: pip install termcolor
+from ThresholdFct import *
+from MorphologicalOperationFct import *
 
 
 img = cv2.imread("Metal_Structure.png", 0)
@@ -16,6 +17,18 @@ img = cv2.imread("Metal_Structure.png", 0)
 hist = histogram(img)
 threshold = threshold(hist)
 thresholded_img = get_thresholded_img(img, threshold)
+print("thresholding" + colored(" done", "green"))
 
-cv2.imshow("thresholded image", thresholded_img) #to see the result
+#cv2.imshow("thresholded image", thresholded_img) #to see the result of thresholding
+
+
+kernel = np.ones((3, 3), np.uint8) #kernel used by ImageJ
+eroded_img = erode(thresholded_img, kernel, 2)
+print("erosion" + colored(" done", "green"))
+
+dilated_img = dilate(eroded_img, kernel, 2)
+print("dilation" + colored(" done", "green"))
+
+#cv2.imshow("eroded image", eroded_img) #to see the result of erosion
+cv2.imshow("dilated image", dilated_img) #to see the result of dilation
 cv2.waitKey(0)
