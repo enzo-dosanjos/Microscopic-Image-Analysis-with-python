4794efb62a7485acfda6fc09221290a93dd25824
Srelox <58371885+Srelox@users.noreply.github.com>
2023-01-27 22:55:56 +0100

    some modification

--- ImageAnalysisMain.py
+++ ImageAnalysisMain.py
@@ -5,32 +5,29 @@ Created on Tue Jan 24 21:45:05 2023
 @author: Enzo && Sacha
 """
 
-import cv2 #to install opencv: pip install opencv-python
-from termcolor import colored #to install termcolor: pip install termcolor
+import cv2  # to install opencv: pip install opencv-python
+from termcolor import colored  # to install termcolor: pip install termcolor
 from ThresholdFct import *
 from MorphologicalOperationFct import *
 from GaussianBlurFct import *
 
-
 img = cv2.imread("test.png", 0)
 
-cv2.imshow("original image", img) #to see the original image
-
+cv2.imshow("original image", img)  # to see the original image
 
 gaussian_kernel = np.array(
     [
-    [1, 4, 7, 4, 1],
-    [4, 16, 26, 16, 4],
-    [7, 26, 41, 26, 7],
-    [4, 16, 26, 16, 4],
-    [1, 4, 7, 4, 1]
+        [1, 4, 7, 4, 1],
+        [4, 16, 26, 16, 4],
+        [7, 26, 41, 26, 7],
+        [4, 16, 26, 16, 4],
+        [1, 4, 7, 4, 1]
     ]
 )
 blured_img = blur(img, gaussian_kernel, 1)
-print("bluring" + colored(" done", "green"))
+print("blurring" + colored(" done", "green"))
 
-cv2.imshow("blured image", blured_img) #to see the result of blur
-
+cv2.imshow("blured image", blured_img)  # to see the result of blur
 
 hist = histogram(img)
 print("histogram" + colored(" plotted", "green"))
@@ -41,18 +38,26 @@ print("threshold" + colored(" found", "green"))
 thresholded_img = thresholding(img, threshold)
 print("thresholding" + colored(" done", "green"))
 
-cv2.imshow("thresholded image", thresholded_img) #to see the result of thresholding
+cv2.imshow("thresholded image", thresholded_img)  # to see the result of thresholding
 
-
-kernel = np.ones((3, 3), np.uint8) #kernel used by ImageJ
-eroded_img = erode(thresholded_img, kernel, 2)
+kernel = np.ones((3, 3), np.uint8)  # kernel used by ImageJ
+eroded_img = erode(thresholded_img, kernel, 3)
 print("erosion" + colored(" done", "green"))
 
-cv2.imshow("eroded image", eroded_img) #to see the result of erosion
-
+cv2.imshow("eroded image", eroded_img)  # to see the result of erosion
 
 dilated_img = dilate(eroded_img, kernel, 2)
 print("dilation" + colored(" done", "green"))
 
-cv2.imshow("dilated image", dilated_img) #to see the result of dilation
+cv2.imshow("dilated1 image", dilated_img)  # Maybe use some blur ?
+
+"""
+eroded_img = erode(thresholded_img, kernel, 2)
+print("erosion2" + colored(" done", "green"))
+
+dilated_img = dilate(eroded_img, kernel, 2)
+print("dilation2" + colored(" done", "green"))
+"""
+
+cv2.imshow("dilated image2", dilated_img)  # to see the result of dilation
 cv2.waitKey(0)
