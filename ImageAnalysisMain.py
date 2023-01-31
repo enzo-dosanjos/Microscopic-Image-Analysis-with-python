8662a213e86344305c633065c4c293c01c3c06fe
enzo-dosanjos <colinterz@gmail.com>
2023-01-31 19:31:42 +0100

    implementation of the properties function

--- ImageAnalysisMain.py
+++ ImageAnalysisMain.py
@@ -10,11 +10,13 @@ from termcolor import colored  # to install termcolor: pip install termcolor
 from ThresholdFct import *
 from MorphologicalOperationFct import *
 from GaussianBlurFct import *
-import FloodAlgo as fl
+from FloodAlgo import *
+from PropertiesFct import *
 
 img = cv2.imread("Metal_Structure.png", 0)
 
-cv2.imshow("original image", img)  # to see the original image
+#cv2.imshow("original image", img)  # to see the original image
+
 
 gaussian_kernel = np.array(
     [
@@ -29,36 +31,47 @@ gaussian_kernel = np.array(
 blured_img = blur(img, gaussian_kernel, 1)
 print("blurring" + colored(" done", "green"))
 
-cv2.imshow("blured image", blured_img)  # to see the result of blur
+#cv2.imshow("blured image", blured_img)  # to see the result of blur
 
+"""
 hist = histogram(img)
 print("histogram" + colored(" plotted", "green"))
 
 threshold = otsu_threshold(hist, img)
 print(f"threshold of {threshold}" + colored(" found", "green"))
-
-thresholded_img = thresholding(img, threshold)
+"""
+thresholded_img = thresholding(img, 158)
 print("thresholding" + colored(" done", "green"))
 
-cv2.imshow("thresholded image", thresholded_img)  # to see the result of thresholding
+#cv2.imshow("thresholded image", thresholded_img)  # to see the result of thresholding
+
 
 kernel = np.ones((3, 3), np.uint8)  # kernel used by ImageJ
 eroded_img = erode(thresholded_img, kernel, 2)
 print("erosion" + colored(" done", "green"))
 
-cv2.imshow("eroded image", eroded_img)  # to see the result of erosion
+#cv2.imshow("eroded image", eroded_img)  # to see the result of erosion
+
 
 dilated_img = dilate(eroded_img, kernel, 2)
 print("dilation" + colored(" done", "green"))
 
-cv2.imshow("dilated image2", dilated_img)
-
-
-imgflooded = fl.floodrgb(dilated_img, [255, 255, 255])
+cv2.imshow("dilated image", dilated_img)
 
 
 
-cv2.imshow("flooded", imgflooded[0])
+flooded_img, nb_cells, color_list = floodrgb(dilated_img, [255, 255, 255])
+
+cv2.imshow("flooded", flooded_img)
+
+
+#get the properties of each cells
+pixels_to_m = 500e-9 #to define
+
+for i, colors in enumerate(color_list):
+    area, perimeter, height, width = get_properties(flooded_img, colors)
+    print(f"cell {i} : - Area : {area} pixels - Perimeter : {perimeter} pixels - Height : {height} pixels - Width : {width} pixels")
+
 
 """
 eroded_img = erode(thresholded_img, kernel, 2)
@@ -68,6 +81,5 @@ dilated_img = dilate(eroded_img, kernel, 2)
 print("dilation2" + colored(" done", "green"))
 """
 
-  # to see the result of dilation
 cv2.waitKey(0)
 
--- WantedResult.py
+++ WantedResult.py
@@ -19,7 +19,7 @@ pixels_to_m = 500e-9 #to define
 
 #clean image
 blured_image = cv2.GaussianBlur(img,(5,5),0)
-cv2.imshow("blured image", blured_image)
+#cv2.imshow("blured image", blured_image)
 
 #plt.hist(img.flat, bins = 100, range=(0,255)) #to determine the threshold value manually
 img_return, img_thresholded = cv2.threshold(blured_image, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU) #return the recommanded threshold for the image and put the value 255 (black) to the pixels corresponding to the grains border and 0 (white) for the others
@@ -34,7 +34,6 @@ img_dilated = cv2.dilate(img_eroded, kernel, iterations=2) #then dilate to get t
 #cv2.imshow("eroded image", img_eroded)
 #cv2.imshow("Thresholded Image", img_thresholded)
 #cv2.imshow("Dilated Image", img_dilated) #to test the result of thresholding, erosion and dilate
-cv2.waitKey(0)
 
 
 #create mask
@@ -47,11 +46,10 @@ labeled_mask, nb_labels = ndimage.label(mask, structure = [[1, 1, 1], [1, 1, 1],
 
 #img_labelled = color.label2rgb(labeled_mask, bg_label = 0)
 #cv2.imshow("colored label image", img_labelled) #to verify the result of the labeled mask
-#cv2.waitKey(0)
+cv2.waitKey(0)
 
-"""
+
 #measure the properties of each grains
 clusters = measure.regionprops(labeled_mask, img) #properties: Area, Orientation, Perimeter, Major/MinorAxisLenght
 for properties in clusters:
     print(f"label : {properties.label} Area : {properties.area} Perimeter : {properties.perimeter}")
-"""
